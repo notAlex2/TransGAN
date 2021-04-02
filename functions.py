b519073ae4eb2b89dfb05323386def009cb80e3e
Alexander Peplowski <alexander.peplowski@gmail.com>
2021-04-01 23:04:33 -0400

    WIP: malloc: corrupted size vs. prev_size

--- functions.py
+++ functions.py
@@ -44,13 +44,15 @@ def cur_stages(iter, args):
 def compute_gradient_penalty(D, real_samples, fake_samples, phi):
     """Calculates the gradient penalty loss for WGAN GP"""
     # Random weight term for interpolation between real and fake samples
-    alpha = torch.Tensor(np.random.random((real_samples.size(0), 1, 1, 1)))
+    alpha = torch.Tensor(
+        np.random.random((real_samples.size(0), 1, 1, 1))
+    )  # .to("cpu")
     # Get random interpolation between real and fake samples
     interpolates = (alpha * real_samples + ((1 - alpha) * fake_samples)).requires_grad_(
         True
     )
     d_interpolates = D(interpolates)
-    fake = torch.ones([real_samples.shape[0], 1], requires_grad=False)
+    fake = torch.ones([real_samples.shape[0], 1], requires_grad=False)  # .to("cpu")
     # Get gradient w.r.t. interpolates
     gradients = torch.autograd.grad(
         outputs=d_interpolates,
@@ -60,7 +62,10 @@ def compute_gradient_penalty(D, real_samples, fake_samples, phi):
         retain_graph=True,
         only_inputs=True,
     )[0]
-    gradients = gradients.view(gradients.size(0), -1)
+    # TODO: try removing reshape, contiguous()
+    gradients = gradients.reshape(gradients.size(0), -1)
+    # gradients = gradients.view(gradients.size(0), -1)
+    # gradients = gradients.contiguous().view(gradients.size(0), -1)
     gradient_penalty = ((gradients.norm(2, dim=1) - phi) ** 2).mean()
     return gradient_penalty
 
