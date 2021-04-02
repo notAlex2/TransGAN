c0188cffee38714db8a09b73b9ac89482add5163
Alexander Peplowski <alexander.peplowski@gmail.com>
2021-04-01 20:14:55 -0400

    WIP training loop

--- test.py
+++ test.py
@@ -11,7 +11,14 @@ from __future__ import print_function
 import cfg
 import models
 import datasets
-from functions import train, validate, LinearLrDecay, load_params, copy_params, cur_stages
+from functions import (
+    train,
+    validate,
+    LinearLrDecay,
+    load_params,
+    copy_params,
+    cur_stages,
+)
 from utils.utils import set_log_dir, save_checkpoint, create_logger
 from utils.inception_score import _init_inception
 from utils.fid_score import create_inception_graph, check_or_download_inception
@@ -24,19 +31,21 @@ from tensorboardX import SummaryWriter
 from tqdm import tqdm
 from copy import deepcopy
 from adamw import AdamW
-import random 
+import random
 
 torch.backends.cudnn.enabled = True
 torch.backends.cudnn.benchmark = True
 
 from collections import OrderedDict
+
 # https://discuss.pytorch.org/t/solved-keyerror-unexpected-key-module-encoder-embedding-weight-in-state-dict/1686/12
 def fix_state_dict(state_dict):
     new_state_dict = OrderedDict()
     for k, v in state_dict.items():
-        name = k[7:] # remove module.
+        name = k[7:]  # remove module.
         new_state_dict[name] = v
     return new_state_dict
+
 
 def main():
     args = cfg.parse_args()
@@ -46,68 +55,85 @@ def main():
     random.seed(args.random_seed)
     torch.backends.cudnn.deterministic = True
 
-
     # set tf env
     _init_inception()
     inception_path = check_or_download_inception(None)
     create_inception_graph(inception_path)
 
     # import network
-    gen_net = eval('models.'+args.gen_model+'.Generator')(args=args).cpu()
-    dis_net = eval('models.'+args.dis_model+'.Discriminator')(args=args).cpu()
+    gen_net = eval("models." + args.gen_model + ".Generator")(args=args).cpu()
+    dis_net = eval("models." + args.dis_model + ".Discriminator")(args=args).cpu()
     gen_net.set_arch(args.arch, cur_stage=2)
 
     # weight init
     def weights_init(m):
         classname = m.__class__.__name__
-        if classname.find('Conv2d') != -1:
-            if args.init_type == 'normal':
+        if classname.find("Conv2d") != -1:
+            if args.init_type == "normal":
                 nn.init.normal_(m.weight.data, 0.0, 0.02)
-            elif args.init_type == 'orth':
+            elif args.init_type == "orth":
                 nn.init.orthogonal_(m.weight.data)
-            elif args.init_type == 'xavier_uniform':
-                nn.init.xavier_uniform(m.weight.data, 1.)
+            elif args.init_type == "xavier_uniform":
+                nn.init.xavier_uniform(m.weight.data, 1.0)
             else:
-                raise NotImplementedError('{} unknown inital type'.format(args.init_type))
-        elif classname.find('BatchNorm2d') != -1:
+                raise NotImplementedError(
+                    "{} unknown inital type".format(args.init_type)
+                )
+        elif classname.find("BatchNorm2d") != -1:
             nn.init.normal_(m.weight.data, 1.0, 0.02)
             nn.init.constant_(m.bias.data, 0.0)
-    
+
     gen_net.apply(weights_init)
     dis_net.apply(weights_init)
 
     # gpu_ids = [i for i in range(int(torch.cuda.device_count()))]
-    # gen_net = torch.nn.DataParallel(gen_net.to("cpu:0"), device_ids=None)
-    # dis_net = torch.nn.DataParallel(dis_net.to("cpu:0"), device_ids=None)
+    gen_net = torch.nn.DataParallel(gen_net.to("cpu:0"), device_ids=None)
+    dis_net = torch.nn.DataParallel(dis_net.to("cpu:0"), device_ids=None)
 
-    # gen_net.module.cur_stage = 0
-    # dis_net.module.cur_stage = 0
-    # gen_net.module.alpha = 1.
-    # dis_net.module.alpha = 1.
+    gen_net.module.cur_stage = 0
+    dis_net.module.cur_stage = 0
+    gen_net.module.alpha = 1.0
+    dis_net.module.alpha = 1.0
 
     # set optimizer
     if args.optimizer == "adam":
-        gen_optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, gen_net.parameters()),
-                                        args.g_lr, (args.beta1, args.beta2))
-        dis_optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, dis_net.parameters()),
-                                        args.d_lr, (args.beta1, args.beta2))
+        gen_optimizer = torch.optim.Adam(
+            filter(lambda p: p.requires_grad, gen_net.parameters()),
+            args.g_lr,
+            (args.beta1, args.beta2),
+        )
+        dis_optimizer = torch.optim.Adam(
+            filter(lambda p: p.requires_grad, dis_net.parameters()),
+            args.d_lr,
+            (args.beta1, args.beta2),
+        )
     elif args.optimizer == "adamw":
-        gen_optimizer = AdamW(filter(lambda p: p.requires_grad, gen_net.parameters()),
-                                        args.g_lr, weight_decay=args.wd)
-        dis_optimizer = AdamW(filter(lambda p: p.requires_grad, dis_net.parameters()),
-                                         args.g_lr, weight_decay=args.wd)
-    gen_scheduler = LinearLrDecay(gen_optimizer, args.g_lr, 0.0, 0, args.max_iter * args.n_critic)
-    dis_scheduler = LinearLrDecay(dis_optimizer, args.d_lr, 0.0, 0, args.max_iter * args.n_critic)
+        gen_optimizer = AdamW(
+            filter(lambda p: p.requires_grad, gen_net.parameters()),
+            args.g_lr,
+            weight_decay=args.wd,
+        )
+        dis_optimizer = AdamW(
+            filter(lambda p: p.requires_grad, dis_net.parameters()),
+            args.g_lr,
+            weight_decay=args.wd,
+        )
+    gen_scheduler = LinearLrDecay(
+        gen_optimizer, args.g_lr, 0.0, 0, args.max_iter * args.n_critic
+    )
+    dis_scheduler = LinearLrDecay(
+        dis_optimizer, args.d_lr, 0.0, 0, args.max_iter * args.n_critic
+    )
 
     # fid stat
-    if args.dataset.lower() == 'cifar10':
-        fid_stat = 'fid_stat/fid_stats_cifar10_train.npz'
-    elif args.dataset.lower() == 'stl10':
-        fid_stat = 'fid_stat/stl10_train_unlabeled_fid_stats_48.npz'
+    if args.dataset.lower() == "cifar10":
+        fid_stat = "fid_stat/fid_stats_cifar10_train.npz"
+    elif args.dataset.lower() == "stl10":
+        fid_stat = "fid_stat/stl10_train_unlabeled_fid_stats_48.npz"
     elif args.fid_stat is not None:
         fid_stat = args.fid_stat
     else:
-        raise NotImplementedError(f'no fid stat for {args.dataset.lower()}')
+        raise NotImplementedError(f"no fid stat for {args.dataset.lower()}")
     assert os.path.exists(fid_stat)
 
     # epoch number for dis_net
@@ -125,54 +151,78 @@ def main():
 
     # set writer
     if args.load_path:
-        print(f'=> resuming from {args.load_path}')
+        print(f"=> resuming from {args.load_path}")
         assert os.path.exists(args.load_path)
         checkpoint_file = os.path.join(args.load_path)
         assert os.path.exists(checkpoint_file)
-        checkpoint = torch.load(checkpoint_file, map_location=torch.device('cpu'))
-        start_epoch = checkpoint['epoch']
-        best_fid = checkpoint['best_fid']
-        gen_net.load_state_dict(fix_state_dict(checkpoint['gen_state_dict']))
-        dis_net.load_state_dict(fix_state_dict(checkpoint['dis_state_dict']))
-        gen_optimizer.load_state_dict(checkpoint['gen_optimizer'])
-        dis_optimizer.load_state_dict(checkpoint['dis_optimizer'])
+        checkpoint = torch.load(checkpoint_file, map_location=torch.device("cpu"))
+        start_epoch = checkpoint["epoch"]
+        best_fid = checkpoint["best_fid"]
+        gen_net.load_state_dict(fix_state_dict(checkpoint["gen_state_dict"]))
+        dis_net.load_state_dict(fix_state_dict(checkpoint["dis_state_dict"]))
+        gen_optimizer.load_state_dict(checkpoint["gen_optimizer"])
+        dis_optimizer.load_state_dict(checkpoint["dis_optimizer"])
         avg_gen_net = deepcopy(gen_net)
-        avg_gen_net.load_state_dict(fix_state_dict(checkpoint['avg_gen_state_dict']))
+        avg_gen_net.load_state_dict(fix_state_dict(checkpoint["avg_gen_state_dict"]))
         gen_avg_param = copy_params(avg_gen_net)
         del avg_gen_net
         cur_stage = cur_stages(start_epoch, args)
-        # gen_net.module.cur_stage = cur_stage
-        # dis_net.module.cur_stage = cur_stage
-        # gen_net.module.alpha = 1.
-        # dis_net.module.alpha = 1.
+        gen_net.module.cur_stage = cur_stage
+        dis_net.module.cur_stage = cur_stage
+        gen_net.module.alpha = 1.0
+        dis_net.module.alpha = 1.0
 
         # args.path_helper = checkpoint['path_helper']
-        
+
     else:
         # create new log dir
+        cur_stage = cur_stages(start_epoch, args)
+        gen_net.module.cur_stage = cur_stage
+        dis_net.module.cur_stage = cur_stage
+        gen_net.module.alpha = 1.0
+        dis_net.module.alpha = 1.0
         assert args.exp_name
-    args.path_helper = set_log_dir('logs', args.exp_name)
-    logger = create_logger(args.path_helper['log_path'])
+    args.path_helper = set_log_dir("logs", args.exp_name)
+    logger = create_logger(args.path_helper["log_path"])
 
     logger.info(args)
     writer_dict = {
-        'writer': SummaryWriter(args.path_helper['log_path']),
-        'train_global_steps': start_epoch * len(train_loader),
-        'valid_global_steps': start_epoch // args.val_freq,
+        "writer": SummaryWriter(args.path_helper["log_path"]),
+        "train_global_steps": start_epoch * len(train_loader),
+        "valid_global_steps": start_epoch // args.val_freq,
     }
 
+    epoch = 0
+
     # train loop
-        
-    epoch = 300
+    train(
+        args,
+        gen_net,
+        dis_net,
+        gen_optimizer,
+        dis_optimizer,
+        gen_avg_param,
+        train_loader,
+        epoch,
+        writer_dict,
+        fixed_z,
+        schedulers=None,
+    )
+
+    # epoch = 300
     backup_param = copy_params(gen_net)
     load_params(gen_net, gen_avg_param)
-    fid_score = validate(args, fixed_z, fid_stat, epoch, gen_net, writer_dict, )
-    logger.info(f'FID score: {fid_score} || @ epoch {epoch}.')
+    fid_score = validate(
+        args,
+        fixed_z,
+        fid_stat,
+        epoch,
+        gen_net,
+        writer_dict,
+    )
+    logger.info(f"FID score: {fid_score} || @ epoch {epoch}.")
     load_params(gen_net, backup_param)
 
 
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
